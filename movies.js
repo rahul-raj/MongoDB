cc4950f3825fb5447912b42322242f859293b6b5
Rahul Raj <omrahulrajcse@gmail.com>
2018-03-17 02:30:52 -0400

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+
+ {$match: {
+     languages: {$elemMatch : {$eq: "English"}},
+     "imdb.rating":{$gte: 1},
+     "imdb.votes":{$gte: 1},
+     year: {$gte: 1990}
+ }},
+ {$project:
+      {"imdb.rating": 1,title:1}
+     
+  },
+ {$addFields:
+	 {scaled_votes: {$add: [1, {$multiply: [9, {$divide: [{$subtract:["$imdb.votes",5]},{$subtract: [1521105,5]}]}]}]},
+	       
+	 }
+   
+ },
+ {$addFields: 
+    {           
+		normalized_rating: {$avg:["$scaled_votes","$imdb.rating"]}
+	}
+  },
+  {$project: {scaled_votes: 0}},
+  {$sort: {normalized_rating: 1}},
+  {$limit: 1}
+  
+
+])
+
